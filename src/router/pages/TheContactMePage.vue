bd5628c154c694aeb597d4f85e7061639c1712c5
behail <angbehailu@gmail.com>
2022-10-21 15:40:28 +0300

    window desktop view

--- src/router/pages/TheContactMePage.vue
+++ src/router/pages/TheContactMePage.vue
@@ -1,47 +1,65 @@
 <template>
-  <section class=" border-1 border-gray-400 block md:flex md:justify-between rounded-md
-   mt-2 md:mt-14 ml-0  md:ml-40 drop-shadow-lg">
-    <div class="p-1 md:p-2 pb-2 md:pb-4 w-2/2 md:w-1/2 ">
-      <h3 class=" uppercase font-bold text-2xl m-2 md:m-10">Contact Me!</h3>
-      <div class=" mt-5 md:mt-32 text-sm md:text-3xl">
+  <section
+    class="border-1 border-gray-400 block md:flex md:justify-between rounded-md mt-2 md:mt-10 ml-0 md:ml-40 drop-shadow-lg"
+  >
+    <div class="p-1 md:p-2 pb-2 md:pb-4 w-2/2 md:w-1/2">
+      <h3 class="uppercase font-bold text-2xl m-2 md:m-10">Contact Me!</h3>
+      <div class="mt-5 md:mt-32 text-sm md:text-3xl">
         <em class="fa fa-envelope text-pinksh pr-0 md:pr-3 mr-0"></em>
-        <span class="pl-0 ml-1 md:ml-2 p-1 md:p-2 text-white text-xl md:text-3xl">
-          angbehailu@gmail.com</span>
-          <p class=" p-1 md:p-2 pl-14 md:pl-24 text-white text-xl md:text-3xl">
-            hbehailu1234@gmail.com</p>
+        <span
+          class="pl-0 ml-1 md:ml-2 p-1 md:p-2 text-white text-xl md:text-3xl"
+        >
+          angbehailu@gmail.com</span
+        >
+        <p class="p-1 md:p-2 pl-14 md:pl-24 text-white text-xl md:text-3xl">
+          hbehailu1234@gmail.com
+        </p>
       </div>
-      <div class=" text-md md:text-3xl mt-2 md:mt-4">
+      <div class="text-md md:text-3xl mt-2 md:mt-4">
         <i class="fa fa-phone text-pinksh" aria-hidden="true"></i>
-          <a class="pl-2 md:pl-6 pr-12 md:pr-20 p-2 text-white text-xl md:text-3xl"
-          href="tel:+251 900-650-493">
-            +251 900-650-493</a>
+        <a
+          class="pl-2 md:pl-6 pr-12 md:pr-20 p-2 text-white text-xl md:text-3xl"
+          href="tel:+251 900-650-493"
+        >
+          +251 900-650-493</a
+        >
       </div>
-      <div class=" flex justify-center mb-4 md:mb-0 mt-8 md:mt-24 items-center">
+      <div class="flex justify-center mb-4 md:mb-0 mt-8 md:mt-24 items-center">
         <div>
           <a href="https://github.com/behail" target="blank">
-            <em class="fa fa-github text-2xl hover:text-white hover:rotate-90"></em></a>
+            <em
+              class="fa fa-github text-2xl hover:text-white hover:rotate-90"
+            ></em
+          ></a>
         </div>
         <div>
-          <a href="https://www.linkedin.com/in/behailu-mesganaw-706390118" target="blank">
-            <em class="fa fa-linkedin pl-4 text-2xl hover:text-white"></em></a>
+          <a
+            href="https://www.linkedin.com/in/behailu-mesganaw-706390118"
+            target="blank"
+          >
+            <em class="fa fa-linkedin pl-4 text-2xl hover:text-white"></em
+          ></a>
         </div>
         <div>
-          <a href="https://twitter.com/behailuM1234?t=jLc4FfeSwsyTzh7UVkHrDw&s=09" target="blank">
-            <em class="fa fa-twitter pl-4 text-2xl hover:text-white"></em></a>
+          <a
+            href="https://twitter.com/behailuM1234?t=jLc4FfeSwsyTzh7UVkHrDw&s=09"
+            target="blank"
+          >
+            <em class="fa fa-twitter pl-4 text-2xl hover:text-white"></em
+          ></a>
         </div>
       </div>
     </div>
 
-    <div class=" w-2/2 md:w-1/2">
+    <div class="w-2/2 md:w-1/2">
       <TheForm />
     </div>
   </section>
-
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import TheForm from '@/components/TheForm.vue';
+import { defineComponent } from "vue";
+import TheForm from "@/components/TheForm.vue";
 
 export default defineComponent({
   components: {
--- src/router/pages/TheLandingPage.vue
+++ src/router/pages/TheLandingPage.vue
@@ -1,91 +1,124 @@
 <template>
   <div>
-  <figure class="md:flex  md:p-10 p-0">
-      <img class="  ml-10 p-2 md:p-12 md:w-37 md:h-50 md:rounded-tr-lg
-      rounded-full w-60 mx-auto mt-0 "
-            src="../../assets/icons/bm1.png" alt="" />
+    <figure class="lg:flex md:p-10 p-0">
+      <img
+        class="ml-10 p-2 md:p-12 md:w-37 md:h-30 md:rounded-tr-lg rounded-full w-60 mx-auto mt-0"
+        src="../../assets/icons/bm1.png"
+        alt=""
+      />
       <div class="p-0 md:p-20 text-center md:text-left space-y-0 md:space-y-4">
-        <blockquote class="p-0 mr-3 pt-0 md:pt-28 pb-2">
-          <p class="text-lg font-medium text-white">
-            HI THERE! I'M</p> <p class="text-pinksh uppercase italic text-xl pb-4">
-            <strong>{{ name }}</strong></p>
-             <p class="text-white text-lg"> A <strong>{{profession}}</strong> passionate about
-              creating interactive applications and experiences on the web.
-            </p>
+        <blockquote class="p-0 mr-3 pt-0 md:pt-2 pb-2">
+          <p class="text-lg font-medium text-white">HI THERE! I'M</p>
+          <p class="text-pinksh uppercase italic text-xl pb-4">
+            <strong>{{ name }}</strong>
+          </p>
+          <p class="text-white text-lg">
+            A <strong>{{ profession }}</strong> passionate about creating
+            interactive applications and experiences on the web.
+          </p>
         </blockquote>
-        <div class="flex justify-center md:justify-start w-50 h-auto ml-20 ">
-          <a href="../../assets/pdf/BM_CV_Oct.pdf" download
-          class="  bg-pinksh text-white font-semibold hover:bg-white hover:text-pinksh
-           uppercase px-8 py-2 border-1 border-white rounded-md">
-            Download Resume</a>
+        <div class="flex justify-center md:justify-start w-50 h-auto ml-20">
+          <a
+            href="../../assets/pdf/BM_CV_Oct.pdf"
+            download
+            class="bg-pinksh text-white font-semibold hover:bg-white hover:text-pinksh uppercase px-8 py-2 border-1 border-white rounded-md"
+          >
+            Download Resume</a
+          >
         </div>
         <!-- <pdfViewer :fileUrl="`../../assets/pdf/BM_CV_Oct.pdf`" /> -->
-        <Carousel :autoplay="4000" :itemsToShow="3" :wrap-around="true"
-        class=" px-4  w-72 md:w-96 pt-12">
-            <Slide class="p-2 pl-5" v-for="item in items" :key="item.title">
-              <img class="rounded-full w-5 md:w-10 h-5 md:h-10 p-1" :src="item.icon"
-              alt="vue.js" />
-              <p class="text-white text-sm pr-3">{{item.title}}</p>
-            </Slide>
-          </Carousel>
+        <Carousel
+          :autoplay="4000"
+          :itemsToShow="3"
+          :wrap-around="true"
+          class="px-4 w-72 md:w-96 pt-12"
+        >
+          <Slide class="p-2 pl-5" v-for="item in items" :key="item.title">
+            <img
+              class="rounded-full w-5 md:w-10 h-5 md:h-10 p-1"
+              :src="item.icon"
+              alt="vue.js"
+            />
+            <p class="text-white text-sm pr-3">{{ item.title }}</p>
+          </Slide>
+        </Carousel>
         <div class="flex justify-end">
-          <h1 class="text-slate-500 text-center font-light uppercase text-xs
-          md:text-sm pr-1">Skills</h1>
+          <h1
+            class="text-slate-500 text-center font-light uppercase text-xs md:text-sm pr-1"
+          >
+            Skills
+          </h1>
           <div class="flex justify-center text-slate-500 pr-2">
-            <em class="fa fa-cog fa-spin fa-1x" ></em>
+            <em class="fa fa-cog fa-spin fa-1x"></em>
           </div>
         </div>
-
       </div>
     </figure>
 
-    <div class="text-white flex flex-row justify-center items-center bottom-0 w-full mt-14 md:mt-0">
-      <p class=" uppercase text-xs text-slate-500 italic">Reach out to me on</p>
-        <a class=" pl-2 uppercase font-light text-gray-400 text-sm underline hover:text-pinksh"
-          v-for="social in socials" :key="social" :href="social.url" target="_blank">
-          {{ social.name }}</a>
+    <div
+      class="text-white flex flex-row justify-center items-center bottom-0 w-full mt-0 md:mt-0"
+    >
+      <p class="uppercase text-xs text-slate-500 italic">Reach out to me on</p>
+      <a
+        class="pl-2 uppercase font-light text-gray-400 text-sm underline hover:text-pinksh"
+        v-for="social in socials"
+        :key="social"
+        :href="social.url"
+        target="_blank"
+      >
+        {{ social.name }}</a
+      >
     </div>
   </div>
 </template>
 
-<script  lang="ts">
-import { defineComponent, ref } from 'vue';
-import { Carousel, Slide } from 'vue3-carousel';
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { Carousel, Slide } from "vue3-carousel";
 // import pdfViewer from '../../components/pdfViewer.vue';
-import 'vue3-carousel/dist/carousel.css';
+import "vue3-carousel/dist/carousel.css";
 
 export default defineComponent({
-  name: 'Auto-play',
+  name: "Auto-play",
   components: {
     Carousel,
     Slide,
     // pdfViewer,
   },
   setup() {
-    const name = ref('Behailu Mesganaw');
-    const profession = ref('Frontend Web Developer');
+    const name = ref("Behailu Mesganaw");
+    const profession = ref("Frontend Web Developer");
     const topSkills = ref<string[]>([
-      'ReactJs',
-      'VueJs',
-      'Typescript',
-      'HTML5',
-      'TailWind',
+      "ReactJs",
+      "VueJs",
+      "Typescript",
+      "HTML5",
+      "TailWind",
     ]);
-    const socials = ref<object[]>([{ url: 'https://www.linkedin.com/in/behailu-mesganaw-706390118', name: 'Linkdin' },
-      { url: 'https://github.com/behail', name: 'Github' }]);
-    const imageSrc: string = '../../assets/icons/css.png';
+    const socials = ref<object[]>([
+      {
+        url: "https://www.linkedin.com/in/behailu-mesganaw-706390118",
+        name: "Linkdin",
+      },
+      { url: "https://github.com/behail", name: "Github" },
+    ]);
+    const imageSrc: string = "../../assets/icons/css.png";
     const items = [
-      { title: 'Vue', icon: require('../../assets/icons/vue.png') },
-      { title: 'React', icon: require('../../assets/icons/react.png') },
-      { title: 'TypeScript', icon: require('../../assets/icons/ts.png') },
-      { title: 'JavaScript', icon: require('../../assets/icons/js.png') },
-      { title: 'HTML', icon: require('../../assets/icons/html.png') },
-      { title: 'CSS', icon: require('../../assets/icons/css.png') },
+      { title: "Vue", icon: require("../../assets/icons/vue.png") },
+      { title: "React", icon: require("../../assets/icons/react.png") },
+      { title: "TypeScript", icon: require("../../assets/icons/ts.png") },
+      { title: "JavaScript", icon: require("../../assets/icons/js.png") },
+      { title: "HTML", icon: require("../../assets/icons/html.png") },
+      { title: "CSS", icon: require("../../assets/icons/css.png") },
     ];
     return {
-      name, profession, topSkills, socials, imageSrc, items,
+      name,
+      profession,
+      topSkills,
+      socials,
+      imageSrc,
+      items,
     };
   },
 });
-
 </script>
